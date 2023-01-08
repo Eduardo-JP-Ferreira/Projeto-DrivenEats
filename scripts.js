0c05e4b34f6b11b0d35efaf765e784974c4c6002
Eduardo Ferreira <eduardopires2014@gmail.com>
2023-01-08 13:56:37 -0300

    Upgrade Botao e Java

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let IconeAnterior;
+let nomeComida;
 let comida;
 function SelecionarPrato(cliqueUm){
 
@@ -8,6 +8,8 @@ function SelecionarPrato(cliqueUm){
         botaoselecUm.classList.remove('seleciona');        
     }
     cliqueUm.classList.add('seleciona');
+    let nome = document.querySelector('.prata .seleciona h1');
+    nomeComida = nome.innerHTML;
     comida =1;
     HabilitarBotao();
 
@@ -20,15 +22,18 @@ function SelecionarPrato(cliqueUm){
     IconeAnterior = 1;*/
     
 }
+let nomeBebida;
 let bebida;
 function SelecionarBebida(cliqueDois){
 
-    const botaoselecDois = document.querySelector('.bebida .borda');
+    const botaoselecDois = document.querySelector('.bebida .seleciona');
     if (botaoselecDois !== null){
-        botaoselecDois.classList.remove('borda');  
+        botaoselecDois.classList.remove('seleciona');  
     }
   
-    cliqueDois.classList.add('borda');
+    cliqueDois.classList.add('seleciona');
+    let nome = document.querySelector('.bebida .seleciona h1');
+    nomeBebida = nome.innerHTML;
     bebida = 1;
     HabilitarBotao();
   /*  const iconeUm = document.querySelector('.bebida .none');
@@ -39,15 +44,18 @@ function SelecionarBebida(cliqueDois){
         iconeUm.classList.remove('none'); 
     }*/
 }
+let nomeSobremesa;
 let sobremesa;
 function SelecionarSobremesa(cliqueTres){
 
-    const botaoselecTres = document.querySelector('.sobremesa .borda');
+    const botaoselecTres = document.querySelector('.sobremesa .seleciona');
     if (botaoselecTres !== null){
-        botaoselecTres.classList.remove('borda');  
+        botaoselecTres.classList.remove('seleciona');  
     }
 
-    cliqueTres.classList.add('borda');
+    cliqueTres.classList.add('seleciona');
+    let nome = document.querySelector('.sobremesa .seleciona h1');
+    nomeSobremesa = nome.innerHTML;
     sobremesa=1;
     HabilitarBotao();
    // cliqueTres.classList.remove('none');
@@ -55,15 +63,18 @@ function SelecionarSobremesa(cliqueTres){
 
 function HabilitarBotao(){
     console.log(comida);
+    console.log(nomeComida);
     console.log(bebida);
+    console.log(nomeBebida);
     console.log(sobremesa);
+    console.log(nomeSobremesa);
     if (comida !== undefined){
         
         if (bebida !== undefined){
 
             if (sobremesa !== undefined){
                 const teste = document.querySelector(".botao");
-                teste.innerHTML = "Continuar";
+                teste.innerHTML = "Fechar pedido";
                 teste.classList.add('verde');
                 document.getElementById("botao_1").removeAttribute("disabled");
             }
@@ -73,5 +84,12 @@ function HabilitarBotao(){
 
 function BotaoClicado(clicado){
     alert("Clicou hahaha");
+    Mensagem();
 }
 
+function Mensagem(){
+   
+    let mensagem = "Ola tio Phil";
+    encodeURIComponent(mensagem);
+    window.open("https://wa.me/55999999999?text=mensagem");
+}
